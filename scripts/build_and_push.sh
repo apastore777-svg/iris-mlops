c6a7382d48ca8cefc24ad9416380bbbfdba7c8bd
Armando Pastore <apastore777-svg@github.com>
2026-03-23 19:21:06 -0300

    improve ecr build script

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -2,20 +2,23 @@
 
 set -e
 
-ACCOUNT=494722910828
-REGION=us-east-2
+echo "🚀 Starting build and push..."
+
+# 🔧 CONFIG
+ACCOUNT=${ACCOUNT_ID:-$(aws sts get-caller-identity --query Account --output text)}
+REGION=${AWS_DEFAULT_REGION:-us-east-2}
 IMAGE=iris-model
 REPO=$ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$IMAGE
 
-TAG=$(date +%s)
+# 🔥 TAG INTELIGENTE
+TAG=${MODEL_VERSION:-$(date +%s)}
 
-echo "Logging into AWS ECR..."
-
+echo "🔐 Logging into AWS ECR..."
 aws ecr get-login-password --region $REGION | \
 docker login --username AWS \
 --password-stdin $ACCOUNT.dkr.ecr.$REGION.amazonaws.com
 
-echo "Ensuring ECR repository exists..."
+echo "📦 Ensuring ECR repository exists..."
 
 aws ecr describe-repositories \
 --repository-names $IMAGE \
@@ -24,22 +27,22 @@ aws ecr create-repository \
 --repository-name $IMAGE \
 --region $REGION
 
-echo "Building Docker image..."
+echo "🐳 Building Docker image..."
 
 docker build \
 -t $IMAGE \
 -f serving/api/Dockerfile \
 .
 
-echo "Tagging image..."
+echo "🏷 Tagging image..."
 
 docker tag $IMAGE $REPO:$TAG
 docker tag $IMAGE $REPO:latest
 
-echo "Pushing image..."
+echo "📤 Pushing image..."
 
 docker push $REPO:$TAG
 docker push $REPO:latest
 
-echo "Image pushed successfully:"
+echo "✅ Image pushed successfully:"
 echo "$REPO:$TAG"
